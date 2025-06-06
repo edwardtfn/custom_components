9cfbad0416c2227020a9d10c32781a035bdd77a8
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-06-06 07:08:05 +0200

    Revert ifan04 schema

--- components/ifan04/__init__.py
+++ components/ifan04/__init__.py
@@ -1,10 +1,10 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation
-from esphome.components import fan, uart
+from esphome.components import uart
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ['uart']
-
 
 ifan04_ns = cg.esphome_ns.namespace('ifan04')
 IFan04 = ifan04_ns.class_('IFan04', cg.Component, uart.UARTDevice)
@@ -13,23 +13,17 @@ CONF_ON_FAN = "on_fan"
 CONF_ON_LIGHT = "on_light"
 CONF_ON_BUZZER = "on_buzzer"
 
-CONFIG_SCHEMA = (
-    fan.fan_schema(IFan04)
-    .extend({
-        cv.Optional(CONF_ON_FAN): automation.validate_automation(single=True)
-    })
-    .extend({
-        cv.Optional(CONF_ON_LIGHT): automation.validate_automation(single=True)
-    })
-    .extend({
-        cv.Optional(CONF_ON_BUZZER): automation.validate_automation(single=True)
-    })
-    .extend(uart.UART_DEVICE_SCHEMA)
+# Fixed: Use cv.COMPONENT_SCHEMA instead of fan.fan_schema since IFan04 is not a Fan
+CONFIG_SCHEMA = cv.All(
+    cv.COMPONENT_SCHEMA.extend({
+        cv.Optional(CONF_ON_FAN): automation.validate_automation(single=True),
+        cv.Optional(CONF_ON_LIGHT): automation.validate_automation(single=True),
+        cv.Optional(CONF_ON_BUZZER): automation.validate_automation(single=True),
+    }).extend(uart.UART_DEVICE_SCHEMA)
 )
 
-
 async def to_code(config):
-    var = await fan.new_fan(config)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
@@ -45,4 +39,3 @@ async def to_code(config):
         await automation.build_automation(
             var.get_buzzer_trigger(), [], config[CONF_ON_BUZZER]
         )
-
