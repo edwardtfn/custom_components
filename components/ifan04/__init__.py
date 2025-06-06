5b75fa2469787e34e6bfe1fac47a18d1fc8fe152
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-06-06 06:59:24 +0200

    Fix schema

--- components/ifan04/__init__.py
+++ components/ifan04/__init__.py
@@ -13,12 +13,13 @@ CONF_ON_FAN = "on_fan"
 CONF_ON_LIGHT = "on_light"
 CONF_ON_BUZZER = "on_buzzer"
 
-CONFIG_SCHEMA = ({
+CONFIG_SCHEMA = (
     fan.fan_schema(IFan04)
-    cv.Optional(CONF_ON_FAN): automation.validate_automation(single=True),
-    cv.Optional(CONF_ON_LIGHT): automation.validate_automation(single=True),
-    cv.Optional(CONF_ON_BUZZER): automation.validate_automation(single=True),
-}).extend(uart.UART_DEVICE_SCHEMA)
+    .extend(cv.Optional(CONF_ON_FAN): automation.validate_automation(single=True))
+    .extend(cv.Optional(CONF_ON_LIGHT): automation.validate_automation(single=True))
+    .extend(cv.Optional(CONF_ON_BUZZER): automation.validate_automation(single=True))
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
 
 
 async def to_code(config):
